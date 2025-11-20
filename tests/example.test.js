9391635e7864e05ff30d45be916b83b15eb310bc
qvietle <viet.le@tuni.fi>
2025-11-20 20:02:12 +0200

    add tests

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,5 +1,5 @@
 import { assert } from 'chai';
-
+import { add } from '../src/example.js';
 
 // For now some random incomplete
 describe('Hello World', () => {
@@ -9,6 +9,10 @@ describe('Hello World', () => {
 
     it('must say Hello Universe', () => {
         assert.strictEqual('hello universe', 'hello universe');
+    })
+
+    it('must add two numbers', () => {
+        assert.strictEqual(add(2, 3), 5);
     })
 
 
