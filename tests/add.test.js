a904a813c58776054c9ccc906ed0aa0d48c41335
TucTam <pkhiettam@gmail.com>
2025-11-29 20:32:41 +0200

    add.test.js logic fixed

--- tests/add.test.js
+++ tests/add.test.js
@@ -50,10 +50,14 @@ describe('Testing Add Function', () => {
         expect(add()).to.equal(0);
     });
 
-    it('add() should return a number (0) if inputted a wrong params types', () => {
-        expect(add('a', 1)).to.be.a('number', 'expected the result of add() to be a number').and.to.equal(0);
+    it('add() should return a number if one param has a wrong type', () => {
+        expect(add('a', 1)).to.be.a('number', 'expected the result of add() to be a number').and.to.equal(1);
+        expect(add([1, 2, 3], 1)).be.a('number', 'expected the result of add() to be a number').and.to.equal(1);
+        expect(add({ 'a': b }, 1)).be.a('number', 'expected the result of add() to be a number').and.to.equal(1);
+    });
+
+
+    it('add() should throw an error if all params wrong', () => {
         expect(add('a', 'b')).be.a('number', 'expected the result of add() to be a number').and.to.equal(0);
-        expect(add([1, 2, 3], 1)).be.a('number', 'expected the result of add() to be a number').and.to.equal(0);
-        expect(add({ 'a': b }, 1)).be.a('number', 'expected the result of add() to be a number').and.to.equal(0);
     });
 })
